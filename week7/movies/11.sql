dbe004b5626f9c13b280144c247dcd8c71dfca5d
Matt Hayball <mhayball@icloud.com>
2022-11-21 16:14:10 +1100

    Create 11.sql

---
+++ week7/movies/11.sql
@@ -0,0 +1,4 @@
+SELECT movies.title
+FROM movies, stars, ratings, people
+WHERE movies.id = stars.movie_id AND stars.person_id = people.id AND ratings.movie_id = movies.id AND people.name = "Chadwick Boseman"
+ORDER BY ratings.rating DESC LIMIT 5;
